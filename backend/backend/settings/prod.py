8b0f435f1246f56a559dd66731ec2ea0f288253a
miyoung <noonchicat@gmail.com>
2021-05-21 11:22:23 +0900

    azure와 연동시키기

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,1 +1,11 @@
+from os import environ
 from .common import *
+
+STATICFILES_STORAGE = "backend.storages.StaticAzureStorage"
+DEFAULT_FILE_STORAGE = "ackend.storages.MediaAzureStorage"
+
+# AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
+# AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
+
+AZURE_ACCOUNT_NAME = "popupbook"
+AZURE_ACCOUNT_KEY = "dvew3XPkK9zqJehdae9QxrEvclZfIxEvEZJ/d5KnR6c31AROuq7l/zxtWPj+lXOdnSd4kMcM0slU6DzEKgwvlg=="
